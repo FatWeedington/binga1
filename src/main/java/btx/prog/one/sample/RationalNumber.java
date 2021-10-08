82000ed2558bdbc330ae753dd4300a4745a346d1
fatweedington <aron.binggeli@gmail.com>
2021-10-08 11:14:57 +0200

    TestCommit

--- src/main/java/btx/prog/one/sample/RationalNumber.java
+++
@@ -1,6 +0,0 @@
-package btx.prog.one.sample;
-
-public class RationalNumber {
-
-
-}
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-/*
- * (c) 2021 by Eric Dubuis - BFH BTX8041, Programming 1
- */
-module btx.prog.one.exes {
-    requires transitive javafx.controls;
-    exports btx.prog.one.sample;
-}
