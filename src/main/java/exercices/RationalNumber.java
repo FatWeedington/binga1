f053670a1a4111765fa488913c434ff9e8ec8595
fatweedington <aron.binggeli@gmail.com>
2021-10-17 13:18:07 +0200

    Lösung Aufgabe 2

--- src/main/java/exercices/RationalNumber.java
+++ src/main/java/exercices/RationalNumber.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class RationalNumber implements Comparable<RationalNumber> {
-
     private long numerator;
     private long denominator;
 
