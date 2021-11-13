c38b10a78ccc8fa50aa9bf2e2b84dbc09a1148bc
fatweedington <aron.binggeli@gmail.com>
2021-11-13 10:24:41 +0100

    testclass modified, Javadoc added

--- src/main/java/exercices/RationalNumber.java
+++ src/main/java/exercices/RationalNumber.java
@@ -1,18 +1,47 @@
 package exercices;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+
+/**
+ * <p>Class which represents any form of rational number displayed by two Integer Numbers</p>
+ *  * @author Aron Binggeli
+ *  * @version 1.0
+ */
 
 public class RationalNumber implements Comparable<RationalNumber> {
     private long numerator;
     private long denominator;
 
+    /**
+     *<p>Represents a rational number with the value of -1</p>
+     */
+
     public static final RationalNumber NEGATIVEONE = new RationalNumber(-1);
+
+    /**
+     *<p>Represents a rational number with the value 0 </p>
+     */
+
     public static final RationalNumber ZERO = new RationalNumber(0);
+
+    /**
+     *<p>Represents a rational number with the value 1 </p>
+     */
+
     public static final RationalNumber ONE = new RationalNumber(1);
+
+    /**
+     *<p>Represents a rational number with the value 2 </p>
+     */
+
     public static final RationalNumber TWO = new RationalNumber(2);
-    public static final RationalNumber TEN = new RationalNumber(10);
+
+    /**
+     * <p>Constructor which creates new object of Type RationalNumber with the two paramaters, each represented by a number from type Long</p>
+     * @param numerator Long
+     * @param denominator Long
+     */
 
     public RationalNumber(long numerator, long denominator) {
         long gcd = gcd(numerator,denominator);
@@ -27,23 +56,52 @@ public class RationalNumber implements Comparable<RationalNumber> {
             this.numerator = -this.numerator;
         }
     }
+
+    /**
+     * <p>Constructor which creates an Object of the type RationalNumber the paramater is represented by a number type long, the denominator is always set to 1 <p/>
+     * @param value Long
+     */
+
     public RationalNumber(long value) {
         this.numerator = value;
         this.denominator = 1;
     }
+
+    /**
+     * <p>returns a new object from Type RationalNumber representing the mathematical addidtion from own object from Type RationalNumber with another</p>
+     * @param other RationalNumber
+     * @return RationalNumber
+     */
+
     public RationalNumber add(RationalNumber other) {
         return new RationalNumber((other.denominator * this.numerator)+(this.denominator * other.numerator),this.denominator*other.denominator);
     }
 
+    /**
+     * <p>returns a new object from Type RationalNumber representing the mathematical multiplication from own object from Type RationalNumber with another</p>
+     * @param other RationalNumber
+     * @return RationalNumber
+     */
+
     public RationalNumber multiply(RationalNumber other) {
         return new RationalNumber(this.numerator*other.numerator,this.denominator*other.denominator);
     }
+
+    /**
+     * <p>returns an integer value from -1 to 1 depending if the own Object is >/=/< 0</p>
+     * @return Int
+     */
 
     public int signum(){
         if (this.numerator < 0){return -1;}
         if (this.numerator == 0){return 0;}
         return 1;
     }
+
+    /**
+     * <p>returns the absolute Value of the own RationalNumber Object</p>
+     * @return RationalNumber
+     */
 
     public RationalNumber abs(){
         if (this.numerator < 0){
@@ -52,13 +110,29 @@ public class RationalNumber implements Comparable<RationalNumber> {
         return this;
     }
 
+    /**
+     * <p>returns a RationalNumber which represents the own object multipiled by -1</p>
+     * @return RationalNumber
+     */
+
     public RationalNumber negate(){
         return this.multiply(NEGATIVEONE);
     }
 
+    /**
+     * <p>Returns a new Object of the Type RationalNumber, which is the result of the subtraction of own minus other RationalNumber</p>
+     * @param other RationalNumber Object
+     * @return RationalNumber
+     */
+
     public RationalNumber subtract(RationalNumber other) {
         return this.add(other.negate());
     }
+
+    /**
+     * <p>Returns a new Object of Type RationalNumber which represents the reciprocal of given RationalNumber</p>
+     * @return RationalNumber
+     */
 
     public RationalNumber oneOver(){
         if (this.numerator == 0){
@@ -68,120 +142,65 @@ public class RationalNumber implements Comparable<RationalNumber> {
         long numerator = this.denominator;
         return new RationalNumber(numerator,denominator);
     }
+
+    /**
+     * <p>returns a new object from Type RationalNumber representing the mathematical division from own object from Type RationalNumber with another</p>
+     * @param other RationalNumber
+     * @return Rationalnumber
+     */
+
     public RationalNumber divide(RationalNumber other) {
             return this.multiply(other.oneOver());
     }
 
+    /**
+     * <p>Returns a new Object of type RationalNumber which represents the result of the number powered by the Integer value exponent</p>
+     * @param exponent long
+     * @return RationalNumber
+     */
+
     public RationalNumber power(long exponent){
        if (exponent >= 0){
            return new RationalNumber((long)Math.pow(this.numerator,exponent),(long)Math.pow(this.denominator,exponent));
-        }
+       }
        return this.oneOver().power(-exponent);
     }
+
+    /**
+     * <p>returns an Integer value by dividing the numerator by the denominator</p>
+     * @return Int
+     */
+    
     public int toInt(){return (int) this.toLong();}
+
+    /**
+     * <p>returns a long value by dividing the numerator by the denominator</p>
+     * @return Long
+     */
     public long toLong(){return this.numerator/this.denominator;}
+
+    /**
+     * <p>returns a Double value by dividing the numerator by the denominator</p>
+     * @return Double
+     */
     public double toDouble(){return 1.0* this.numerator/this.denominator;}
 
-    public static RationalNumber sum(List<RationalNumber> numbers){
-        RationalNumber sum = RationalNumber.ZERO;
-        for (RationalNumber number : numbers) {
-            sum = sum.add(number);
-        }
-        return sum;
+
+    /**
+     * <p>Compares two given RationalNumber Objects by outputting 1, if this>o, a 0 if this=o or -1 if this<o </p>
+     * @param o RationalNumber
+     * @return Int
+     */
+    @Override
+    public int compareTo(RationalNumber o) {
+        // return Double.compare(this.toDouble(),o.toDouble());
+        return this.subtract(o).signum();
     }
 
-    public static RationalNumber average(List<RationalNumber> numbers){
-        if (numbers.size() == 0)
-                return null;
-      return RationalNumber.sum(numbers).divide(new RationalNumber(numbers.size()));
-    }
-
-    public static RationalNumber max(List<RationalNumber> numbers) {
-        return Collections.max(numbers);
-    }
-
-    public static RationalNumber min(List<RationalNumber> numbers) {
-        return Collections.min(numbers);
-    }
-
-    private static long gcd(long n1 ,long  n2) {
-          if ( n2 == 0) {
-            return n1 ;
-        }
-        return gcd ( n2 , n1 % n2 ) ;
-    }
-
-    public static RationalNumber valueof(double value) {
-        for (long i= 1; i < Long.MAX_VALUE;i*=10){
-            if (i*value % 1 == 0)
-                return new RationalNumber((long)(i*value),i);
-        }
-           return null;
-    }
-
-    public static RationalNumber valueof(String valueStr) {
-       valueStr = valueStr.trim();
-       if (valueStr.contains("."))
-           return RationalNumber.valueof(Double.valueOf(valueStr));
-       if (!valueStr.contains("/")){
-            return new RationalNumber(Long.valueOf(valueStr));
-        }
-       
-        String[] values = valueStr.split("/");
-        return new RationalNumber(Long.valueOf(values[0]),Long.valueOf(values[1]));
-    }
-
-    public static RationalNumber eval(String expression){
-
-        if (expression.contains("+")){
-            String[] subExpressions = expression.split("\\+");
-            RationalNumber sum = eval(subExpressions[0]);
-            for(int i = 1 ; i < subExpressions.length;i++){
-                RationalNumber value =eval(subExpressions[i]);
-                sum = sum.add(value);}
-            return sum;
-        }
-
-        if (expression.contains("_")){
-            String[] subExpressions = expression.split("_");
-            RationalNumber diff = eval(subExpressions[0]);
-            for(int i = 1 ; i < subExpressions.length;i++){
-                RationalNumber value =eval(subExpressions[i]);
-                diff = diff.subtract(value);}
-            return diff;
-        }
-
-        if (expression.contains("*")){
-            String[] subExpressions = expression.split("\\*");
-            RationalNumber product = eval(subExpressions[0]);
-            for(int i = 1 ; i < subExpressions.length;i++){
-                RationalNumber value =eval(subExpressions[i]);
-                product = product.multiply(value);}
-            return product;
-        }
-
-        if (expression.contains(":")){
-            String[] subExpressions = expression.split("\\:");
-            RationalNumber quot = eval(subExpressions[0]);
-            for(int i = 1 ; i < subExpressions.length;i++){
-                RationalNumber value =eval(subExpressions[i]);
-                quot = quot.divide(value);}
-            return quot;
-        }
-
-        if (expression.contains("^")){
-            String[] subExpressions = expression.split("\\^");
-            RationalNumber pot = eval(subExpressions[0]);
-            for(int i = 1 ; i < subExpressions.length;i++){
-                RationalNumber value =eval(subExpressions[i]);
-                pot = pot.power(value.toLong());}
-            return pot;
-        }
-
-                return RationalNumber.valueof(expression);
-            }
-
-
+    /**
+     * <p>Returns a String in the form numerator/denominator </p>
+     * @return String
+     */
     @Override
     public String toString() {
         if (this.denominator == 1) {
@@ -193,11 +212,16 @@ public class RationalNumber implements Comparable<RationalNumber> {
         return this.numerator + "/" + this.denominator;
     }
 
+    /**
+     * <p>Returns True if both RationalNumber Objects are Equal, and FALSE if they are not</p>
+     * @param o RationalNumber
+     * @return boolean
+     */
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof RationalNumber)) return false;
-        RationalNumber that = (RationalNumber) o;
+        if (!(o instanceof RationalNumber that)) return false;
         return numerator == that.numerator && denominator == that.denominator;
     }
 
@@ -206,11 +230,147 @@ public class RationalNumber implements Comparable<RationalNumber> {
         return Objects.hash(numerator, denominator);
     }
 
-    @Override
-    public int compareTo(RationalNumber o) {
-    // return Double.compare(this.toDouble(),o.toDouble());
-    return this.subtract(o).signum();
+    /**
+     * <p>Takes in a List of Rational Numbers and returns another RationalNumber which represents the sum of all number contained in the List</p>
+     * @param numbers List
+     * @return RationalNumber
+     */
+
+    public static RationalNumber sum(List<RationalNumber> numbers){
+        RationalNumber sum = RationalNumber.ZERO;
+        for (RationalNumber number : numbers) {
+            sum = sum.add(number);
+        }
+        return sum;
+    }
+
+    /**
+     * <p>Takes a list of Objects from Type RationalNumber and returns a new Object from type Rationalnumber, which represents the average of all numbers contained in the given List</p>
+     * @param numbers List
+     * @return RationalNumber
+     */
+    public static RationalNumber average(List<RationalNumber> numbers){
+        if (numbers.size() == 0)
+            return null;
+        return RationalNumber.sum(numbers).divide(new RationalNumber(numbers.size()));
+    }
+
+    /**
+     * <p>Takes a list of Objects from Type RationalNumber and returns the Rationalnumber, which represents the maximum Value of all numbers contained in the given List</p>
+     * @param numbers List
+     * @return RationalNumber
+     */
+    public static RationalNumber max(List<RationalNumber> numbers) {
+        return Collections.max(numbers);
+    }
+
+    /**
+     * <p>Takes a list of Objects from Type RationalNumber and returns the Rationalnumber, which represents the minimum Value of all numbers contained in the given List</p>
+     * @param numbers List
+     * @return RationalNumber
+     */
+    public static RationalNumber min(List<RationalNumber> numbers) {
+        return Collections.min(numbers);
+    }
+
+    private static long gcd(long n1 ,long  n2) {
+        if ( n2 == 0) {
+            return n1 ;
+        }
+        return gcd ( n2 , n1 % n2 ) ;
+    }
+
+    /**
+     * <p>takes an double value input and converts it a rational number</p>
+     * @param value double
+     * @return RationalNumber
+     */
+    public static RationalNumber valueof(double value) {
+        for (long i= 1; i < Long.MAX_VALUE;i*=10){
+            if (i*value % 1 == 0)
+                return new RationalNumber((long)(i*value),i);
+        }
+        return null;
+    }
+
+    /**
+     * <p>takes an String input containing a "/" and splits both of the Values and converts them to a new RationalNumber Object</p>
+     * @param valueStr String
+     * @return RationalNumber
+     */
+    public static RationalNumber valueof(String valueStr) {
+        valueStr = valueStr.trim();
+        if (valueStr.contains("."))
+            return RationalNumber.valueof(Double.parseDouble(valueStr));
+        if (!valueStr.contains("/")){
+            return new RationalNumber(Long.parseLong(valueStr));
+        }
+
+        String[] values = valueStr.split("/");
+        return new RationalNumber(Long.parseLong(values[0]),Long.parseLong(values[1]));
+    }
+
+    /**
+     * <p>Takes a String input, splits it by the signs "+,_,*,:,^" and recalls the method so all operations represented by the signs
+     * are executed by the rule point before line operations</p>
+     * @param expression String
+     * @return RationalNumber
+     */
+    public static RationalNumber eval(String expression){
+
+        if (expression.contains("+")){
+            String[] subExpressions = expression.split("\\+");
+            RationalNumber sum = eval(subExpressions[0]);
+            for(int i = 1 ; i < subExpressions.length;i++){
+                RationalNumber value =eval(subExpressions[i]);
+                sum = sum.add(value);
+            }
+            return sum;
+        }
+
+        if (expression.contains("_")){
+            String[] subExpressions = expression.split("_");
+            RationalNumber diff = eval(subExpressions[0]);
+            for(int i = 1 ; i < subExpressions.length;i++){
+                RationalNumber value =eval(subExpressions[i]);
+                diff = diff.subtract(value);
+            }
+            return diff;
+        }
+
+        if (expression.contains("*")){
+            String[] subExpressions = expression.split("\\*");
+            RationalNumber product = eval(subExpressions[0]);
+            for(int i = 1 ; i < subExpressions.length;i++){
+                RationalNumber value =eval(subExpressions[i]);
+                product = product.multiply(value);
+            }
+            return product;
+        }
+
+        if (expression.contains(":")){
+            String[] subExpressions = expression.split(":");
+            RationalNumber quot = eval(subExpressions[0]);
+            for(int i = 1 ; i < subExpressions.length;i++){
+                RationalNumber value =eval(subExpressions[i]);
+                quot = quot.divide(value);
+            }
+            return quot;
+        }
+
+        if (expression.contains("^")){
+            String[] subExpressions = expression.split("\\^");
+            RationalNumber pot = eval(subExpressions[0]);
+            for(int i = 1 ; i < subExpressions.length;i++){
+                RationalNumber value =eval(subExpressions[i]);
+                pot = pot.power(value.toLong());
+            }
+            return pot;
+        }
+
+        return RationalNumber.valueof(expression);
 
     }
+
 }
 
--- src/test/java/btx/prog/one/sample/TestRationalNumber.java
+++ src/test/java/btx/prog/one/sample/TestRationalNumber.java
@@ -1,21 +1,173 @@
 package btx.prog.one.sample;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import exercices.RationalNumber;
 import org.junit.jupiter.api.Test;
-
-import java.awt.*;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestRationalNumber {
+
+    private RationalNumber a = new RationalNumber(1,2);
+    private RationalNumber b = new RationalNumber(3,2);
+    private RationalNumber c = new RationalNumber(-1,2);
+    private RationalNumber d = new RationalNumber(1,-3);
+
+    private ArrayList<RationalNumber> numbers = new ArrayList<>();
+
+
+
+    @Test
+    public void testRationalNumber(){
+        assertEquals(new RationalNumber(2,1),RationalNumber.TWO);
+        assertEquals(new RationalNumber(2),RationalNumber.TWO);
+        assertEquals(new RationalNumber(2,4),new RationalNumber(4,8));
+        assertThrows(ArithmeticException.class,()->new RationalNumber(1,0));
+    }
+
     @Test
     public void testAdd(){
-        RationalNumber a = new RationalNumber(1,2);
-        RationalNumber b = new RationalNumber(3,2);
+            assertEquals(a.add(b),RationalNumber.TWO);
+            assertEquals(a.add(c),RationalNumber.ZERO);
+    }
 
-        assertEquals(a.add(b),RationalNumber.TWO);
+    @Test
+    public void TestMultiply(){
+        assertEquals(a.multiply(b),new RationalNumber(3,4));
+        assertEquals(a.multiply(c), new RationalNumber(-1,4));
 
     }
 
+    @Test
+    public void TestSignum(){
+        assertEquals(a.signum(),1);
+        assertEquals(c.signum(),-1);
+    }
+
+    @Test
+    public void TestAbs(){
+        assertEquals(a.abs(),new RationalNumber(1,2));
+        assertEquals(c.abs(),new RationalNumber(1,2));
+    }
+
+    @Test
+    public void TestNegate(){
+        assertEquals(a.negate(),new RationalNumber(-1,2));
+        assertEquals(c.negate(),new RationalNumber(1,2));
+    }
+
+    @Test
+    public void TestSubtract(){
+        assertEquals(RationalNumber.NEGATIVEONE,a.subtract(b));
+        assertEquals(RationalNumber.ONE,a.subtract(c));
+    }
+
+    @Test
+    public void TestOneover(){
+        assertEquals(a.oneOver(),RationalNumber.TWO);
+        assertEquals(d.oneOver(),new RationalNumber(-3));
+        assertThrows(ArithmeticException.class, RationalNumber.ZERO::oneOver);
+    }
+
+    @Test
+    public void TestDivide(){
+        assertEquals(a.divide(b),new RationalNumber(1,3));
+        assertEquals(a.divide(c),RationalNumber.NEGATIVEONE);
+        assertThrows(ArithmeticException.class,()->a.divide(RationalNumber.ZERO));
+    }
+
+    @Test
+    public void TestPower(){
+        assertEquals(a.power(2),new RationalNumber(1,4));
+        assertEquals(c.power(2),new RationalNumber(1,4));
+        assertEquals(c.power(3),new RationalNumber(-1,8));
+    }
+
+    @Test
+    public void TestToInt(){
+        assertEquals(2,RationalNumber.TWO.toInt());
+        assertEquals(0,c.toInt());
+        assertEquals(0,d.toInt());
+    }
+
+    @Test
+    public void TestToLong(){
+        assertEquals(2,RationalNumber.TWO.toLong());
+        assertEquals(0,c.toLong());
+        assertEquals(0,d.toLong());
+    }
+
+    @Test
+    public void TestToDouble(){
+        assertEquals(2.0,RationalNumber.TWO.toDouble());
+        assertEquals(-0.5,c.toDouble());
+        assertEquals(-0.3333333333333333,d.toDouble());
+    }
+
+
+    @Test
+    public void TestSum(){
+        numbers.add(a);
+        numbers.add(b);
+        numbers.add(c);
+        numbers.add(d);
+
+        assertEquals(new RationalNumber(7,6),RationalNumber.sum(numbers));
+    }
+
+    @Test
+    public void TestAverage(){
+        numbers.add(a);
+        numbers.add(b);
+        numbers.add(c);
+        numbers.add(d);
+
+        assertEquals(new RationalNumber(7,24),RationalNumber.average(numbers));
+    }
+
+    @Test
+    public void TestMax(){
+        numbers.add(a);
+        numbers.add(b);
+        numbers.add(c);
+        numbers.add(d);
+
+        assertEquals(b,RationalNumber.max(numbers));
+    }
+
+    @Test
+    public void TestMin(){
+        numbers.add(a);
+        numbers.add(b);
+        numbers.add(c);
+        numbers.add(d);
+
+        assertEquals(c,RationalNumber.min(numbers));
+    }
+
+    @Test
+    public void TestValueOf(){
+        assertEquals(new RationalNumber(3,2),RationalNumber.valueof("1.5"));
+        assertEquals(new RationalNumber(3,2),RationalNumber.valueof(1.5));
+        assertEquals(new RationalNumber(3,2),RationalNumber.valueof("3/2"));
+    }
+
+    @Test
+    public void TestEval(){
+        assertEquals(new RationalNumber(3,2),RationalNumber.eval("1.5"));
+        assertEquals(new RationalNumber(27,16),RationalNumber.eval("1.5+3/4_18/5*5/2:4/1^2"));
+    }
+
+    @Test
+    public void TestCompareTo(){
+        assertEquals(-1, a.compareTo(b));
+        assertEquals(0, a.compareTo(a));
+        assertEquals(1, a.compareTo(c));
+    }
+
+    @Test
+    public void TestEquals(){
+        assertTrue(a.equals(new RationalNumber(1,2)));
+        assertFalse(a.equals(b));
+        assertFalse(a.equals(c));
+    }
 }
